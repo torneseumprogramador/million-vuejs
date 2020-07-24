224aa3e79a67bc89756418f95dfed55b80c76f2d
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-07-23 23:44:43 -0300

    aula dia 23/07/2020

---
+++ src/components/app-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="bd-footer text-muted">
+    <div class="container-fluid p-3 p-md-5">
+      <ul class="bd-footer-links">
+        <li><a href="https://github.com/twbs">GitHub Million</a></li>
+        <li><a href="https://twitter.com/getbootstrap">Million vue.js</a></li>
+      </ul>
+      <p>
+        Criado por alunos do projeto torn-se um programador <a href="http://www.torneseumprogramador.com.br/" target="_blank">http://www.torneseumprogramador.com.br/</a>
+      </p>
+    </div>
+  </footer>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Home from './components/app-home.vue'
+import Clientes from './components/app-clientes.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path: "/", component: Home},
+  {path: "/clientes", component: Clientes},
+]
+
+const router = new VueRouter({
+  routes: routes,
+  mode: "history",
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
